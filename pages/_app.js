6dda05e64e5c26762442408817eb07fe7ac59da3
Aswin <me@aswin.sh>
2023-01-24 20:20:52 +0100

    ✨ Added nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,11 @@
 import "../styles/globals.css";
+import Router from "next/router";
+import nProgress from "nprogress";
+import "../styles/nprogress.css";
+
+Router.events.on("routeChangeStart", nProgress.start);
+Router.events.on("routeChangeError", nProgress.done);
+Router.events.on("routeChangeComplete", nProgress.done);
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;

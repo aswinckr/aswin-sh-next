ae1324724cba226c6bdd465a44f8ed47c767042f
Aswin <me@aswin.sh>
2023-01-29 21:54:01 +0100

    🍃 improved links - 2

--- components/Button.tsx
+++ components/Button.tsx
@@ -11,16 +11,15 @@ const Button = (props: ButtonProps) => {
   const { href = "/", type = "secondary", children } = props;
 
   return (
-    <a href={href}>
-      <div
-        className={`pt-2 inline-block cursor-pointer pb-2 pl-4 pr-4 text-white rounded-md font-semibold transition-colors ${
-          type == "primary"
-            ? "bg-gradient-to-r from-purple-400 to-pink-600 text-opacity-100 hover:text-opacity-75"
-            : "border-2 border-primary hover:text-primary"
-        }`}
-      >
-        {children}
-      </div>
+    <a
+      href={href}
+      className={`pt-2 inline-block cursor-pointer pb-2 pl-4 pr-4 text-white rounded-md font-semibold transition-colors ${
+        type == "primary"
+          ? "bg-gradient-to-r from-purple-400 to-pink-600 text-opacity-100 hover:text-opacity-75"
+          : "border-2 border-primary hover:text-primary"
+      }`}
+    >
+      {children}
     </a>
   );
 };

d4db58639767ea0d4148387e13489bfe713d7a76
Aswin <me@aswin.sh>
2023-02-04 18:34:22 +0100

    ✨ updates to short intro

--- components/IntroShort.tsx
+++ components/IntroShort.tsx
@@ -1,26 +1,33 @@
 import React from "react";
 import Avatar from "./Avatar";
+import styles from "../styles/index.module.css";
 
 // font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600
 
 const IntroShort = () => {
   return (
-    <div className="">
+    <div className="mb-24">
       <Avatar />
       <div>
-        <p className="pt-4 text-lg italic opacity-90 leading-loose text-white">
-          Hey there, I'm Aswin!✨, a design manager and an aspiring writer
-          living in Berlin. I'm on a journey to finding my{" "}
-          <a href={`https://perell.com/note/build-a-personal-monopoly/`}>
-            personal monopoly
-          </a>{" "}
-          through writing. You can do it too - recommend checking out{" "}
-          <a href="https://writeofpassage.school/"> write of passage</a> or{" "}
-          <a href="https://www.ship30for30.com/">ship30</a>. This website is my
-          little online home for my ideas. Take a look around, you may find some
-          of them interesting. If you do, leave a comment, or reach out
-          directly, I'm always happy to chat or{" "}
-          <a href="/contact">make a new friend!</a>
+        <p
+          className={`${styles.shortintro} pl-4 italic text-2xl font-bold border-l-4 pt-0 border-gray-600`}
+        >
+          Hello there, I'm Aswin! I manage a team of product designers and I
+          write about a variety of topics like product design, people
+          management, self introspection, productivity and more.
+        </p>
+        <p className={`${styles.shortintro} mt-4 pt-4 pb-4`}>
+          <span className="mr-4"> ⭐️</span>I live now in Berlin, Germany.
+        </p>
+        <p className={styles.shortintro}>
+          <span className="mr-4"> ⭐️</span>I work full time at{" "}
+          <a href="https://www.notion.so">Delivery Hero</a>. I used to work at{" "}
+          <a href="https://www.notion.so">Grab</a> in Singapore before.
+        </p>
+        <p className={`${styles.shortintro} pt-4`}>
+          <span className="mr-4"> ⭐️</span>
+          I'm currently curious about social extroverts and becoming a great
+          communicator. I practice this through improv storytelling.
         </p>
       </div>
     </div>

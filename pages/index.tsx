652a401e0d46d8c83038858db3eb9fc63e3a714c
Aswin <me@aswin.sh>
2023-04-28 09:24:26 +0200

    Minor updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,10 +31,10 @@ export default function Home({ posts }) {
         </div>
         <div className={styles.newsletter}>
           <div className={styles.newsletterWrapper}>
-            <h2>🍻 Sign up for Weekly brew</h2>
+            <h2>🍻 Join my Weekly Brew</h2>
             <p>
               Every week I send a newsletter with my latest writing and things
-              I've been thinking about.
+              I've been thinking about. Sign up below to be added to the list.
             </p>
             <iframe
               src="https://embeds.beehiiv.com/6cb40e0e-448f-40f0-938f-186b8f78b542?slim=true"
